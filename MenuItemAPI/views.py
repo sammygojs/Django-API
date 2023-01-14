bf3cb33d782302ebc0c76ee7e989e57054c006e5
Sumit <akoliyasumit@hotmail.com>
2023-01-14 22:49:16 +0530

    MenuItem retrieve and show simple

--- MenuItemAPI/views.py
+++ MenuItemAPI/views.py
@@ -1,11 +1,8 @@
-from rest_framework import generics
 from .models import MenuItem
-from .serializers import MenuItemSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-class MenuItemsView(generics.ListCreateAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
-
-class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
+@api_view()
+def menu_items(request):
+    items = MenuItem.objects.all()
+    return Response(items.values())
