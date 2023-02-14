ee756ea29df95aab671014eeffe0aac0c6fb0561
Sumit <akoliyasumit@hotmail.com>
2023-02-14 23:08:12 +0530

    drf

--- LittleLemonDRF/apps.py
+++ LittleLemonDRF/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MenuitemapiConfig(AppConfig):
+class LittlelemondrfConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'MenuItemAPI'
+    name = 'LittleLemonDRF'
--- MenuItemAPI/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('menuitems/', views.menu_items),
-    path('menuitem/<int:id>', views.single_item),
-    # path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
-]
