912c3e5ee601672ec4641d31ec5aff9f3d326d0f
Sumit <akoliyasumit@hotmail.com>
2023-02-16 23:46:56 +0530

    relationship serializer m2

--- LittleLemonDRF/serializers.py
+++ LittleLemonDRF/serializers.py
@@ -11,12 +11,13 @@ class MenuItemSerializer(serializers.ModelSerializer):
     stock = serializers.IntegerField(source='inventory')
     price_after_tax = serializers.SerializerMethodField(method_name = 'calculate_tax')
     # category = serializers.StringRelatedField()
-    category = CategorySerializer()
+    # category = CategorySerializer()
     class Meta:
         model = MenuItem
         # fields = ['id','title','price','inventory']
         fields = ['id','title','price','stock','price_after_tax','category']
         # extra_kwargs = {'price':{'min_value':2},'inventory':{'min_value':0}}
+        depth=1
     def calculate_tax(self, product:MenuItem):
         return product.price*Decimal(1.1)
 
