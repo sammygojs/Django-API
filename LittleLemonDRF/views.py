57b5468bbea3bef85189251af653b2e6374f38b0
Sumit <akoliyasumit@hotmail.com>
2023-02-20 21:50:48 +0530

    using paginator

--- LittleLemonDRF/views.py
+++ LittleLemonDRF/views.py
@@ -5,6 +5,8 @@ from .serializers import MenuItemSerializer, CategorySerializer
 from rest_framework import generics, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.core.paginator import Paginator, EmptyPage
+
 
 @api_view(['GET','POST'])
 def menu_items(request):
@@ -14,12 +16,24 @@ def menu_items(request):
         to_price = request.query_params.get('to_price')
         category_name = request.query_params.get('category')
         search = request.query_params.get('search')
+        ordering = request.query_params.get('ordering')
+        perpage = request.query_params.get('perpage',default=2)
+        page = request.query_params.get('page',default=1)
         if category_name:
             items = items.filter(category__title=category_name)
         if to_price:
             items = items.filter(price__lte=to_price)
         if search:
             items = items.filter(title__contains=search)
+        if ordering:
+            ordering_fields = ordering.split(',')
+            items = items.order_by(*ordering_fields)
+
+        paginator = Paginator(items, per_page=perpage)
+        try:
+            items = paginator.page(number=page)
+        except EmptyPage:
+            items = []
         serialized_items = MenuItemSerializer(items, many=True)
         return Response(serialized_items.data)
         # return Response(items.values())
